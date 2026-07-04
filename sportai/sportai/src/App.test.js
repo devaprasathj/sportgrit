768c9e8bd29b5447fbbe80389a41f93fa1e45a3e
devaprasathj <devaprasathdevaprasath57@gmail.com>
2026-07-04 11:43:36 +0530

    Clean duplicate frontend folders

--- sportai/sportai/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app title', () => {
-  render(<App />);
-  const element = screen.getByText(/sport/i);
-  expect(element).toBeInTheDocument();
-});
